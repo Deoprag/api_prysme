e56f3a7a61f08a1b006336bd0fcf7e5c2af4ceeb
Pedro Rocha <pdroesofiarabelo@gmail.com>
2024-08-12 20:16:37 -0300

    initial commit

---
+++ src/main/java/com/deopraglabs/api_prysme/ApiPrysmeApplication.java
@@ -0,0 +1,13 @@
+package com.deopraglabs.api_prysme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPrysmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPrysmeApplication.class, args);
+	}
+
+}
